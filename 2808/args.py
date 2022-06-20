5eaeb8e8293598f47d20abd89e156695a2f2902f
JEAtin <92412717+JEAtin@users.noreply.github.com>
2022-06-20 12:47:18 +0100

    Add files via upload

---
+++ 2808/args.py
@@ -0,0 +1,10 @@
+
+max_length = 128
+seed = 2022
+train_batch_sz = 16
+test_batch_sz = 32
+epochs = 5
+
+data_path = 'data/aclImdb'
+pretrained_model_path = './bert-base-cased'
+output_path = 'output/'
